063d26b1f34b68cde91c24fe1bdd225984180768
Prasad <prasadneeli06@gmail.com>
2019-06-25 21:56:43 +0530

    first commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+    @Test
+    void logintest(){
+        System.out.println("login test");
+    }
+}
---
+++ src/test/java/Test2.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+    @Test
+    void logouttest(){
+        System.out.println("logout test");
+    }
+}
